e1f10803072a5e90b1ae6ee67551b4aed66b633b
karlhass15 <karl@reclaimedartcraftsman.com>
2015-11-03 16:08:19 -0600

    team2

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,11 +1,24 @@
 /**
  * Created by owner on 11/3/15.
  */
+var peopleInfo = {};
 
 
 $(document).ready(function(){
 
 
+    $("#employeeForm").submit(function(event) {
+        event.preventDefault();
+
+
+        $.each($(this).serializeArray(),function (i, field) {
+            peopleInfo[field.name] = field.value;
+
+
+        });
+        console.log(peopleInfo);
+
+    });
 
 });
 
