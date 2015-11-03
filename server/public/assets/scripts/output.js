87c4099977322341a5d040e2237a0cd4b1fa5b31
tehat <thomasehartle@gmail.com>
2015-11-03 16:35:45 -0600

    first divs appear on DOM

--- server/public/assets/scripts/output.js
+++ server/public/assets/scripts/output.js
@@ -1,6 +1,27 @@
 /**
  * Created by Thomas on 11/3/15.
  */
+var employeeArray = [];
+
+
+
+console.log("javascript is working");
 $(document).ready(function(){
+console.log("jquery is working");
+    $.ajax({
+        type: "GET",
+        url: "/output",
+        success: function(output){
+            //populate array with objects
+            //calculate all totals and averages
+
+
+
+            //append to DOM list of employees
+
+
+            console.log("Gathering info for array");
+        }
+    })
 
 })
